9ac4d6a7ab2c93a0a1e39b1ae5a3c89aade19adf
Vinicius <vinicius12sjdr@gmail.com>
2024-03-16 17:43:07 -0300

    Apontando rota raiz para listagem de produtos

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\ProductController::class, 'index']);
 
 Route::group(['prefix' => 'products'], function(){
     Route::get('/', [App\Http\Controllers\ProductController::class, 'index'])->name('products.index');
